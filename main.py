89a47c98f639ea9273badf234b6a2b6a68c52ff5
Syf <sibel.y.filiz@gmail.com>
2021-10-04 06:20:40 +0200

    variables

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hey Wereld! Hey World!")
+name="England"
+age=125
+year, month, day= 2021, "october", 4
+
+array= [345,314,51213,42]
+
+print("name:", name, age)
+print(array)
+print(year,month,day)
