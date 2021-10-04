5aac931fe9d36675d2f153159ac5f5a421195982
Syf <sibel.y.filiz@gmail.com>
2021-10-04 10:52:18 +0200

    datatypes

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 print("Hey Wereld! Hey World!")
-name="England"
-age=125
-year, month, day= 2021, "october", 4
+name ="England"
+age = 125
+year, month, day = 2021, "october", 4
+isTrue = True
+array = [345, 314, 51213, 42]
+PI = 3.14
+piNu = 3.14
 
-array= [345,314,51213,42]
 
 print("name:", name, age)
 print(array)
-print(year,month,day)
+print(year, month, day)
+print(PI)
+print(piNu)
+
+print(type(name))
+print(type(PI))
+print(type(isTrue))
+print(type(day))
+print(type(piNu))
